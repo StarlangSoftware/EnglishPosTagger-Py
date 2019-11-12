3d1774e00e1ce2ce2c29eadd5de277512f3f93bc
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-11-12 08:49:52 +0300

    Updated PosTagger repository translation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-PosTagger',
+    version='1.0.0',
+    packages=['PosTagger'],
+    url='https://github.com/olcaytaner/PosTagger-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='English PosTagger Library'
+)
