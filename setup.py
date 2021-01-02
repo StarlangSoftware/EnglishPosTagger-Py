10e15fe9e13346d330421651b0f9df64d47bbbbe
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:33:23 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-PosTagger',
     version='1.0.5',
     packages=['PosTagger'],
-    url='https://github.com/olcaytaner/PosTagger-Py',
+    url='https://github.com/StarlangSoftware/PosTagger-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='English PosTagger Library',
     install_requires=['NlpToolkit-Corpus', 'NlpToolkit-Hmm']
 )
