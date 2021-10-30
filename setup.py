b25bdad38f0e9f93146111f2ca2be7ce57990fbf
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:45:18 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-PosTagger',
-    version='1.0.5',
+    version='1.0.6',
     packages=['PosTagger'],
     url='https://github.com/StarlangSoftware/PosTagger-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='English PosTagger Library',
-    install_requires=['NlpToolkit-Corpus', 'NlpToolkit-Hmm']
+    install_requires=['NlpToolkit-Corpus', 'NlpToolkit-Hmm'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
